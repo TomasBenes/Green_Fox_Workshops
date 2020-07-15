70252636f01486629b66d311449f5fc40df7858e
TomasBenes <tomasbenes123gmail.com>
2020-07-15 16:26:06 +0200

    updated

--- foundation-example-exam/src/com/company/Animal_protection/AnimalShelter.java
+++ foundation-example-exam/src/com/company/Animal_protection/AnimalShelter.java
@@ -2,17 +2,42 @@ package com.company.Animal_protection;
 
 import java.util.ArrayList;
 
-public class AnimalShelter {
+public class AnimalShelter extends Animal {
 
     int budget = 50;
     ArrayList<Animal> animals = new ArrayList<>();
     ArrayList <String> adoptersName;
 
-    public void rescue (ArrayList animals, Animal animal) {
-        animals.add(animal);
-        System.out.println(animals.size());
+    public int rescue (Animal animal) {
+        this.animals.add(animal);
+        return this.animals.size();
     }
 
     public void heal (){
+    }
+
+    public void addAdopter (String name) {
+        this.adoptersName.add(name);
+    }
+
+    public void findNewOwner () {
+
+    }
+
+    public int earnDonation (int amount) {
+        this.budget += amount;
+        return this.budget;
+    }
+
+    public String toString () {
+        System.out.println("Budget: " + this.budget + "€,");
+        System.out.println("There are " + this.animals.size() + " animal(s) and " + this.adoptersName.size() + " potential adopter(s)");
+        if (isHealthy == false) {
+            String badAnimal = this.name + " is not healthy (" + this.healCost + "€), and not adoptable";
+            return badAnimal;
+        } else {
+            String goodAnimal = this.name + " is healthy, and adoptable";
+            return goodAnimal;
+        }
     }
 }
